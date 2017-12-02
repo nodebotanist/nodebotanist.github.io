49037e50b458ff32fccdfe7e843265ff8379803e
Kas Perch <the@nodebotani.st>
2017-12-02 14:08:26 -0600

    added bootstrap to page

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-export default () => <div>Welcome to nodebotani.st!</div>
+import Head from 'next/head'
+
+export default () =>(
+  <div>
+    <Head>
+      <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <title>My page title</title>
+    </Head>
+
+    <h1>Welcome to Nodebotani.st</h1>
+  </div>
+)
