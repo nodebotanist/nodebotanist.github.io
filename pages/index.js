1141d10c65b4eb3333e3c188f3eb6c7b181c10e3
Kas Perch <the@nodebotani.st>
2017-12-02 17:18:02 -0600

    Added fira font

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Head from 'next/head'
 
-export default () =>(
-  <div>
+export default () => (
+  <div className="container">
     <Head>
       <link rel="stylesheet" href="/static/css/bootstrap.min.css" />
+      <link href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700" rel="stylesheet" />
+      <link rel="stylesheet" href="/static/css/styles.css" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>My page title</title>
     </Head>
 
-    <h1>Welcome to Nodebotani.st</h1>
+    <h1>Welcome to Nodebotani.st!</h1>
   </div>
 )
