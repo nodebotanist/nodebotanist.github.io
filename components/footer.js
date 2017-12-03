83143d9d839f25e28a792341732df19ead8a4b52
Kas Perch <the@nodebotani.st>
2017-12-03 00:31:12 -0600

    Finished footer styling

--- components/footer.js
+++ components/footer.js
@@ -3,55 +3,57 @@ export default () => (
     <div className="row">
       <footer id="footer" className="col-lg-12 nav-container">
         <ul className="list-unstyled list-inline">
+          <li className="text-primary">Social Links:</li>
           <li>
-            <a href="https://twitter.com/nodebotanist">
-              <span className="sr-only">Twitter</span>
-              <i className="fa fa-twitter fa-lg"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://twitch.tv/nodebotanist">
-              <span className="sr-only">Twitch</span>
-              <i className="fa fa-twitch fa-lg"></i>
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/nodebotanist">
+            <a href="https://github.com/nodebotanist" className="text-success">
               <span className="sr-only">GitHub</span>
               <i className="fa fa-github-alt fa-lg"></i>
             </a>
           </li>
           <li>
-            <a href="https://linkedin.com/in/nodebotanist">
-              <span className="sr-only">LinkedIn</span>
-              <i className="fa fa-linkedin-square fa-lg"></i>
+            <a href="https://twitter.com/nodebotanist" className="text-primary">
+              <span className="sr-only">Twitter</span>
+              <i className="fa fa-twitter fa-lg"></i>
             </a>
           </li>
           <li>
-            <a href="https://medium.com/@nodebotanist">
+            <a href="https://twitch.tv/nodebotanist" className="text-info">
+              <span className="sr-only">Twitch</span>
+              <i className="fa fa-twitch fa-lg"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://medium.com/@nodebotanist" className="text-success">
               <span className="sr-only">Medium</span>
               <i className="fa fa-medium fa-lg"></i>
             </a>
           </li>
           <li>
-            <a href="https://open.spotify.com/user/kasperch">
-              <span className="sr-only">Spotify</span>
-              <i className="fa fa-spotify fa-lg"></i>
-            </a>
-          </li>
-          <li>
-            <a href="http://steamcommunity.com/id/nodebotanist/">
+            <a href="http://steamcommunity.com/id/nodebotanist/" className="text-muted">
               <span className="sr-only">Steam</span>
               <i className="fa fa-steam-square fa-lg"></i>
             </a>
           </li>
           <li>
-            <a href="https://www.youtube.com/channel/UC2Md-YpApr3XPTiFhCVrLeA">
+            <a href="https://linkedin.com/in/nodebotanist" className="text-primary">
+              <span className="sr-only">LinkedIn</span>
+              <i className="fa fa-linkedin-square fa-lg"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.youtube.com/channel/UC2Md-YpApr3XPTiFhCVrLeA" className="text-danger">
               <span className="sr-only">YouTube</span>
               <i className="fa fa-youtube-play fa-lg"></i>
             </a>
           </li>
+          <li>
+            <a href="https://open.spotify.com/user/kasperch" className="text-success">
+              <span className="sr-only">Spotify</span>
+              <i className="fa fa-spotify fa-lg"></i>
+            </a>
+          </li>
         </ul>
+        <p>Using the <a href="https://bootswatch.com/cyborg/">Bootswatch Cyborg Theme</a>, and <a href="https://fontawesome.io">Font Awesome</a>.</p>
       </footer>
     </div>
   </div>
