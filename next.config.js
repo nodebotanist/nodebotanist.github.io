7ca2ee9029ac4a9e1edb0ea16ad5847ff1b4e07b
Kas Perch <the@nodebotani.st>
2017-12-05 13:19:25 -0600

    Fixed routes for build

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ module.exports = {
   exportPathMap: function () {
     return {
       '/': { page: '/' },
-      '/about': {page: 'about'},
-      '/bibliography': {page: 'bibliography'},
-      '/bragging': {page: 'bragging'},
-      '/projects': {page: 'projects'}
+      '/about': {page: '/about'},
+      '/bibliography': {page: '/bibliography'},
+      '/bragging': {page: '/bragging'},
+      '/projects': {page: '/projects'}
     }
   }
 }
