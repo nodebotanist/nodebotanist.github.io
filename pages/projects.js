975f1fbd21d157e7f34d1087efa6f2d0b24aad46
Kas Perch <the@nodebotani.st>
2017-12-05 16:24:07 -0600

    Getting started with panels for projects

--- pages/projects.js
+++ pages/projects.js
@@ -7,6 +7,30 @@ export default () => (
     <div className="container">
       <h1>Projects Page</h1>
       <p>I do a lot of random crap, and I put my favorites here. I color-coded them because why not?</p>
+      <p>Color coding is as follows:</p>
+      <ul>
+        <li className="text-warning">Robotics/Hardware Project</li>
+        <li className="text-success">Web Development Project</li>
+        <li className="text-info">Writing Project</li>
+      </ul>
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="panel panel-warning">
+            <div className="panel-heading">
+              <h3 className="panel-title">Title</h3>
+            </div>
+            <div className="panel-body">
+              <p>Test</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-lg-4">
+
+        </div>
+        <div className="col-lg-4">
+
+        </div>
+      </div>
     </div>
     <Footer />
   </div>
