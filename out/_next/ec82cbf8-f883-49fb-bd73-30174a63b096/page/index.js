8d375b387db41152ebbdb525c77002da2ee6425c
Kas Perch <the@nodebotani.st>
2017-12-05 11:20:28 -0600

    Bulid

---
+++ out/_next/ec82cbf8-f883-49fb-bd73-30174a63b096/page/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([5],{184:function(e,t,l){e.exports=l(185)},185:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=l(25),n=a(u),c=l(99),r=a(c),d=l(100),o=a(d);t.default=function(){return n.default.createElement("div",null,n.default.createElement(r.default,null),n.default.createElement("div",{className:"container"},n.default.createElement("h1",null,'"The Nodebotanist"'),n.default.createElement("h4",{className:"text-success"},"(a.k.a. Mx. Kas Perch)"),n.default.createElement("p",null,"Hi! You've reached my portfolio.")),n.default.createElement(o.default,null))}}},[184]);
+            return { page: comp.default }
+          })
+        
