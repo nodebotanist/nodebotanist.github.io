2f3858bc973f27a68e12196817b556e006790380
Kas Perch <the@nodebotani.st>
2017-12-04 23:58:47 -0600

    Added projects page

---
+++ pages/projects.js
@@ -0,0 +1,13 @@
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+export default () => (
+  <div>
+    <Header />
+    <div className="container">
+      <h1>Projects Page</h1>
+
+    </div>
+    <Footer />
+  </div>
+)
