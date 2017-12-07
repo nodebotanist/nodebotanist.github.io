9fafe70320fb86e6d8ab86f4bedbb8f7705f94ba
Kas Perch <the@nodebotani.st>
2017-12-06 21:16:02 -0600

    Projects page update

--- pages/projects.js
+++ pages/projects.js
@@ -20,22 +20,49 @@ export default () => (
               <h3 className="panel-title">Node Bus-Pirate</h3>
             </div>
             <div className="panel-body">
-              <p>So this library was built to control the Bus Pirate with Node.js. Right now it has I2C and UART Functionality. Working on a tester before making progress.</p>
+              <p>This library was built to control the Bus Pirate with Node.js. Right now it has I2C and UART Functionality. Working on a tester before making progress.</p>
               <p>
                 <span className="label label-warning">node-serialport</span>
                 <span className="label label-warning">Bus Pirate</span>
               </p>
             </div>
             <div className="panel-footer">
-              <a href="https://github.com/nodebotanist/node-bus-pirate"><i class="text-success fa fa-github-alt fa-lg"></i></a>
+              <a target="_blank" href="https://github.com/nodebotanist/node-bus-pirate"><i class="text-success fa fa-github-alt fa-lg"></i></a>
             </div>
           </div>
         </div>
         <div className="col-lg-4">
-
+          <div className="panel panel-warning">
+            <div className="panel-heading">
+              <h3 className="panel-title">Bus-Pirate Tester</h3>
+            </div>
+            <div className="panel-body">
+              <p>This is an AVR Firmware project I'm writing so I can run hardware tests on the Bus Pirate for the bus-pirate node module</p>
+              <p>
+                <span className="label label-warning">AVR C++</span>
+                <span className="label label-warning">Bus Pirate</span>
+              </p>
+            </div>
+            <div className="panel-footer">
+              <a target="_blank" href="https://github.com/nodebotanist/node-bus-pirate-tester"><i class="text-success fa fa-github-alt fa-lg"></i></a>
+            </div>
+          </div>
         </div>
         <div className="col-lg-4">
-
+          <div className="panel panel-info">
+            <div className="panel-heading">
+              <h3 className="panel-title">The Nodebotanist's Handbook</h3>
+            </div>
+            <div className="panel-body">
+              <p>Just pulling together advice and such based on my myriad of experience, hoping maybe someone will learn from my mistakes.</p>
+              <p>
+                <span className="label label-info">book</span>
+              </p>
+            </div>
+            <div className="panel-footer">
+              <a target="_blank" href="https://www.gitbook.com/book/nodebotanist/nodebotanist-handbook/details"><i class="text-primary fa fa-book fa-lg"></i></a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
