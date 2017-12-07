8d86c149ff661034248ca579811fde43a7414054
Kas Perch <the@nodebotani.st>
2017-12-06 21:16:30 -0600

    Build

--- out/_next/f4e07d91-2a8a-4d7a-93ab-4db781260766/page/projects/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/projects', function() {
-            var comp = module.exports=webpackJsonp([4],{197:function(e,t,l){e.exports=l(198)},198:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(1),u=a(n),r=l(36),o=a(r),d=l(37),c=a(d);t.default=function(){return u.default.createElement("div",null,u.default.createElement(o.default,null),u.default.createElement("div",{className:"container"},u.default.createElement("h1",null,"Projects Page"),u.default.createElement("p",null,"I do a lot of random crap, and I put my favorites here. I color-coded them because why not?")),u.default.createElement(c.default,null))}}},[197]);
-            return { page: comp.default }
-          })
-        
