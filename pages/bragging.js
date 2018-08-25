72142e88fd7a06cf056a7238704ffb551fb21f7f
Kassandra Perch <the@nodebotani.st>
2018-08-25 17:07:49 -0500

    Updated dependencies, started resume

--- pages/bragging.js
+++ pages/bragging.js
@@ -6,7 +6,31 @@ export default () => (
     <Header />
     <div className="container">
       <h1>Resume (a.k.a. Bragging)</h1>
-
+      <h2>Work History</h2>
+      <h3>Current: DevRel Engineer at IOpipe</h3>
+      <h4>April 2017-Present</h4>
+      <h3>Developer Evangelist at Auth0</h3>
+      <h4>December 2015-December 2016</h4>
+      <h3>Open Software Engineer at Bocoup</h3>
+      <h4>December 2014-November 2015</h4>
+      <h3>Engineering Evangelist at RetailMeNot</h3>
+      <h4>May 2014-October 2014</h4>
+      <h3>Front-End Engineer at RetailMeNot</h3>
+      <h4>April 2013-May 2014</h4>
+      <p>More work history available on request.</p>
+      <h2>Education</h2>
+      <h3>Bachelor's of Science in Computer Science</h3>
+      <h4>St. Edward's University, 2007-2011</h4>
+      <h3>B.S.E. in Electrical Engineering</h3>
+      <h4>Arizona State University, 2016-Present</h4>
+      <h2>Published Works</h2>
+      <h3>Make: Javascript Robotics (Co-Author)</h3>
+      <h4>Make Press, 2015</h4>
+      <h3>Learning JavaScript Robotics</h3>
+      <h4>Packt Publishing, 2015</h4>
+      <h3>Hands-On JavaScript Robotics (Upcoming)</h3>
+      <h4>Packt Publishing, Q4 2018</h4>
+      <h2>Extracurriculars</h2>
     </div>
     <Footer />
   </div>
