49eda3f2db835429d300b7bc42924036e4ddb3a1
Kassandra Perch <the@nodebotani.st>
2018-09-21 00:15:01 -0400

    JS

--- pages/set.js
+++ pages/set.js
@@ -11,8 +11,10 @@ export default withRouter((props) => (
       <p>Hi! You sent the color {props.router.query.color} to the bot!.</p>
 
         <script dangerouslySetInnerHTML={{__html: `
+        let params = new URLSearchParams(document.location.search.substring(1));
+        let color = params.get("color");
             var oReq = new XMLHttpRequest();
-            oReq.open("GET", "https://frbo73bbwg.execute-api.us-east-1.amazonaws.com/default/aws-nodejs-dev-empire2018?color=${props.router.query.color}");
+            oReq.open("GET", "https://frbo73bbwg.execute-api.us-east-1.amazonaws.com/default/aws-nodejs-dev-empire2018?color=${color}");
             oReq.send();`}}>
         </script>
     </div>
