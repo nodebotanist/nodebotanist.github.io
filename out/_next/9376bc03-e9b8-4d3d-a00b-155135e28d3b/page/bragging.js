9fe4caad69176a4989fd3490107024597a77f429
Kassandra Perch <the@nodebotani.st>
2018-09-20 23:58:55 -0400

    Build

--- out/_next/9376bc03-e9b8-4d3d-a00b-155135e28d3b/page/bragging.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/bragging', function() {
-            var comp = module.exports=webpackJsonp([6],{193:function(e,t,l){e.exports=l(194)},194:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(1),a=u(n),r=l(36),d=u(r),c=l(37),f=u(c);t.default=function(){return a.default.createElement("div",null,a.default.createElement(d.default,null),a.default.createElement("div",{className:"container"},a.default.createElement("h1",null,"Resume (a.k.a. Bragging)")),a.default.createElement(f.default,null))}}},[193]);
-            return { page: comp.default }
-          })
-        
