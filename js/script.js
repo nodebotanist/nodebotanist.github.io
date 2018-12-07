1b2013fbe9313230a199a4e34128800cf1826ba3
Kassandra Perch <the@nodebotani.st>
2018-12-07 16:39:54 -0600

    edited it to be me

--- js/script.js
+++ js/script.js
@@ -4,56 +4,58 @@ var bashme = new Bashme.Bashme({
 });
 bashme.use(new Bashme.Json(
   {
-      "name": "Agustín Rodríguez",
-      "bio": "#coder #entrepreneur #optimist #dreamer #student",
+      "name": "nodebotanist (Mx. Kassandra Perch)",
+      "bio": "Developer Advocate Specialist (4 years), Developer (10+ years), IoT/Robotics tinkerer (5 years), EE Student (ASU), Non-Binary (30 years), Creative type (25+ years)",
       "education": [
           {
-              "university": "Universidad ORT Uruguay",
-              "degree": "System's Engineer",
+              "university": "St. Edward's University",
+              "degree": "Bachelor's of Science",
               "fieldOfStudy": "Computer Science",
-              "grade": 90,
-              "startYear": 2014,
-              "endYear": 2019,
-              "notes": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-          }
+              "startYear": 2007,
+              "endYear": 2011,
+              "notes": "Founder and President (2010-2011)/Vice President (2009-2010) of the Loosely Organized Computer Organization."
+          },
+           {
+             "university": "Arizona State University",
+             "degree": "B.S.E.E.",
+             "fieldOfStudy": "Electrical Engineering",
+             "startYear": 2015,
+             "endYear": "???",
+             "notes": "Part-time student. I want to learn how to build my own circuit boards, and learn more about robotics. No plans to switch career paths at this time."
+           }
       ],
       "work": [
-          {
-              "company": "Universidad ORT Uruguay",
-              "position": "Professor Assistant",
-              "startMonth": 7,
-              "startYear": 2016,
-              "endMonth": 12,
-              "endYear": 2016,
-              "description": "Lorem ipsum"
-          }
-      ],
-      "volunteer": [
-          {
-              "organization": "Science Church",
-              "position": "Evangelist",
-              "startMonth": 2,
-              "startYear": 2015,
-              "endMonth": 2,
-              "endYear": 2015,
-              "description": "Fake volunteer entry"
-          }
-      ],
-      "awards": [
-          {
-              "title": "Latin-America First Prize on Innovation",
-              "description": "N/A",
-              "awarder": "http://www.cii.uy",
-              "year": 2016
-          }
+          // {
+              // "company": "Universidad ORT Uruguay",
+              // "position": "Professor Assistant",
+              // "startMonth": 7,
+              // "startYear": 2016,
+              // "endMonth": 12,
+              // "endYear": 2016,
+              // "description": "Lorem ipsum"
+          // }
       ],
       "publications": [
           {
-              "name": "Use of singular names in weird open source code",
-              "description": "Nothing to say",
-              "publisher": "No1",
+              "name": "Getting Started With Javascript Robotics",
+              "description": "How to get started with Raspberry Pi and Johnny-Five",
+              "publisher": "Packt",
               "releaseYear": 2018,
-              "url": "http://agurodriguez.net"
+              "url": ""
+          },
+          {
+            "name": "Learning Javascript Robotics",
+            "description": "How to get started with Arduino and Johnny-Five",
+            "publisher": "Packt",
+            "releaseYear": 2016,
+            "url": ""
+          },
+          {
+            "name": "Make: Javascript Robotics",
+            "description": "I wrote the chapter on RGB LED displays",
+            "publisher": "Make",
+            "releaseYear": 2015,
+            "url": ""
           }
       ],
       "skills": [
@@ -65,16 +67,12 @@ bashme.use(new Bashme.Json(
       ],
       "languages": [
           {
-              "name": "Español",
-              "level": "Native"
-          },
-          {
               "name": "English",
-              "level": "Intermediate"
+              "level": "Native/Fluent"
           }
       ]
   }
 ));
-bashme.use(new Bashme.GitHub('agurodriguez'));
+bashme.use(new Bashme.GitHub('nodebotanist'));
 bashme.show(document.querySelector('#terminal'));
 bashme.on('command', (cmd, args) => console.log(cmd, args));
