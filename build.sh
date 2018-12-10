1ee843112f04426fb50a5bbf4e5681759fddd23d
Kassandra Perch <the@nodebotani.st>
2018-12-10 12:54:15 -0600

    "Build"

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cp ./node_modules/xterm/build/xterm.css ./css &&
 cp ./node_modules/xterm/build/xterm.js ./js &&
-cp ./node_modules/xterm/build/xterm.map.js &&
+cp ./node_modules/xterm/build/xterm.js.map ./js &&
 git add --all && 
 git commit -m \"Build\"
