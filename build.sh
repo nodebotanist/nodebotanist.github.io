d2a560c2cd30f3e6729e672881448023da4448b4
Kassandra Perch <the@nodebotani.st>
2018-12-10 12:47:53 -0600

    Adding xterm

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-cp ./node_modules/xterm/dist/xterm.css ./css &&
+cp ./node_modules/xterm/xterm.css ./css &&
+cp ./node_modules/xterm/xterm.js ./js &&
 git add --all && 
-git commit -m \"Build\" && 
-git push origin dev
+git commit -m \"Build\"
