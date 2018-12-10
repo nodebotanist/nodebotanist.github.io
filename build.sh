86989128a4a5def90e45e991400c19a7a43c13d8
Kassandra Perch <the@nodebotani.st>
2018-12-10 12:57:09 -0600

    Added fit plugin

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 cp ./node_modules/xterm/build/xterm.css ./css &&
 cp ./node_modules/xterm/build/xterm.js ./js &&
 cp ./node_modules/xterm/build/xterm.js.map ./js &&
+cp ./node_modules/xterm/build/addons/fit/fit.js ./js &&
+cp ./node_modules/xterm/build/addons/fit/fit.js.map ./js &&
 git add --all && 
 git commit -m \"Build\"
