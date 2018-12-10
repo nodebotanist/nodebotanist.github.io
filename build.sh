f78ebc37d07e2794dd7515140702de099b8af6ad
Kassandra Perch <the@nodebotani.st>
2018-12-10 12:31:51 -0600

    Removed bashme for xterm

--- build.sh
+++ build.sh
@@ -1,5 +1,4 @@
-cp ./node_modules/bashme/dist/xterm.css ./css && 
-cp ./node_modules/bashme/dist/bundle.js ./js && 
+cp ./node_modules/xterm/dist/xterm.css ./css &&
 git add --all && 
 git commit -m \"Build\" && 
 git push origin dev
--- publish.sh
+++ publish.sh
@@ -1,13 +1,7 @@
-git push origin --delete master && 
-git branch -D master && git checkout --orphan master && 
-rm -rf * && 
-git checkout dev && 
-git reset --hard HEAD && 
-echo \"nodebotani.st\" > out/CNAME && 
-git add out/CNAME && git commit -m \"Add CNAME\" && 
-git subtree push --prefix out origin master && 
-rm out/CNAME && 
-git fetch origin master && 
-git checkout master && 
-git checkout dev && 
-git reset --hard origin/dev && npm i
+git checkout master &&
+git rebase dev &&
+echo \"nodebotani.st\" > CNAME && 
+git add CNAME && 
+git commit -m \"Add CNAME\" && 
+git push origin master && 
+rm CNAME
