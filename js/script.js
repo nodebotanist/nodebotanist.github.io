e48bf3ce71755ab86a3b54192985752fa75b215f
Kassandra Perch <the@nodebotani.st>
2018-12-11 20:20:10 -0600

    Basic key collection

--- js/script.js
+++ js/script.js
@@ -1,27 +1,47 @@
-Terminal.applyAddon(fit)
+(function() {
 
-let term = new Terminal({})
+  let currentLine = ''
+  let currentLineLength = 0
 
-term.open(document.getElementById('#terminal'))
+  Terminal.applyAddon(fit)
 
-term.prompt = () => {
-  term.write('\r\n$ ')
-}
+  let term = new Terminal({})
 
-term.fit()
-term.prompt()
+  term.open(document.getElementById('#terminal'))
 
-term._core.register(term.addDisposableListener('key', (key, ev) => {
-  const printable = !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey;
-
-  if (ev.keyCode === 13) {
-    term.prompt();
-  } else if (ev.keyCode === 8) {
-    // Do not delete the prompt
-    if (term.x > 2) {
-      term.write('\b \b');
-    }
-  } else if (printable) {
-    term.write(key);
+  term.prompt = () => {
+    term.write('\r\n$ ')
   }
-}));
+
+  term.fit()
+  term.prompt()
+
+  term._core.register(term.addDisposableListener('key', (key, ev) => {
+    const printable = !ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey;
+
+    if (ev.keyCode === 13) {
+      term.prompt()
+    } else if (ev.keyCode === 8) {
+      // Do not delete the prompt
+      if (currentLineLength > 0) {
+        term.write('\b \b')
+        currentLineLength--
+        currentLine = currentLine.substring(0, currentLine.length - 1)
+      }
+    } else if (printable) {
+      currentLine = currentLine.concat(key)
+      term.write(key)
+      currentLineLength++
+    }
+
+    console.log('Line: ', currentLine, 'Length: ', currentLineLength)
+  }))
+
+  term._core.register(term.addDisposableListener('paste', (data, ev) => {
+    term.write(data)
+  }))
+
+  term.on('linefeed', (...args) => {
+    console.log(args)
+  })
+})()
