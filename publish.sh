a219f46c73dc02c49c17dafaf290b42fd2850d71
Kassandra Perch <the@nodebotani.st>
2018-12-12 14:04:03 -0600

    Publish script work

--- publish.sh
+++ publish.sh
@@ -1,7 +1,18 @@
-git checkout master &&
-git rebase dev &&
-echo \"nodebotani.st\" > CNAME && 
-git add CNAME && 
-git commit -m \"Add CNAME\" && 
-git push origin master && 
-rm CNAME
+ #! /usr/bash
+
+ git push origin --delete test && 
+ git branch -D test && 
+ git checkout --orphan test && 
+ rm -rf * && 
+ git checkout dev && 
+ git reset --hard HEAD && 
+ echo \"nodebotani.st\" > dist/CNAME && 
+ git add out/CNAME && 
+ git commit -m \"Add CNAME\" && 
+ git subtree push --prefix dist origin test && 
+ rm dist/CNAME && 
+ git fetch origin test && 
+ git checkout test && 
+ git checkout dev && 
+ git reset --hard origin/dev && 
+ npm i
