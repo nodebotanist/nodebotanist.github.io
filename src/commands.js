dc43832e85afea94fad31484b85f7dc355d87916
Kassandra Perch <the@nodebotani.st>
2018-12-12 13:52:04 -0600

    Working fully with webpack!

--- src/commands.js
+++ src/commands.js
@@ -1,30 +1,29 @@
-(function() {
+let printHelp = (...args) => {
+  term.write('HELP')
+}
 
-  let printHelp = (...args) => {
-    term.write('HELP')
+commands = {
+  'help': printHelp
+}
+
+function runCommand(args) {
+  let command = args[0] 
+  let arguments = undefined
+  let commandList = Object.keys(commands)
+
+  if (args.length > 1) {
+    arguments = args.slice(1, command.length)
   }
 
-  commands = {
-    'help': printHelp
+  if (commandList.indexOf(command) === -1) {
+    term.setColor('blue')
+    term.write('Command not found!')
+    term.setColor('reset')
+  } else {
+    commands[command].apply(this, arguments)
   }
+}
 
-  function runCommand(args) {
-    let command = args[0] 
-    let arguments = undefined
-    let commandList = Object.keys(commands)
-
-    if (args.length > 1) {
-      arguments = args.slice(1, command.length)
-    }
-
-    if (commandList.indexOf(command) === -1) {
-      term.setColor('blue')
-      term.write('Command not found!')
-      term.setColor('reset')
-    } else {
-      commands[command].apply(this, arguments)
-    }
-  }
-
-  window.runCommand = runCommand
-})()
+module.exports = {
+  runCommand
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Terminal } from 'xterm'
 import * as fit from 'xterm/lib/addons/fit/fit'
 import * as WebfontLoader from 'xterm-webfont'
+import { runCommand } from './commands'
 
 let currentLine = ''
 let currentLineLength = 0
@@ -74,26 +75,26 @@ term._core.register(term.addDisposableListener('paste', (data, ev) => {
   term.write(data)
 }))
 
-// term.setColor = (colorName) => {
-//   let colorCodes = {
-//     defaultForeground: '\e[39',
-//     black: '\u001b[30m',
-//     red: '\u001b[31m',
-//     green: '\u001b[32m',
-//     yellow: '\u001b[33m',
-//     blue: '\u001b[34m',
-//     magenta: '\u001b[35m',
-//     cyan: '\u001b[36m',
-//     white: '\u001b[37m',
-//     brightBlack: '\u001b[30;1m',
-//     brightRed: '\u001b[31;1m',
-//     brightGreen: '\u001b[32;1m',
-//     brightYellow: '\u001b[33;1m',
-//     brightBlue: '\u001b[34;1m',
-//     brightMagenta: '\u001b[35;1m',
-//     brightCyan: '\u001b[36;1m',
-//     brightWhite: '\u001b[37;1m',
-//     reset: '\u001b[0m',
-//   }
-//   term.write(colorCodes[colorName])
-// }
+term.setColor = (colorName) => {
+  let colorCodes = {
+    defaultForeground: '\e[39',
+    black: '\u001b[30m',
+    red: '\u001b[31m',
+    green: '\u001b[32m',
+    yellow: '\u001b[33m',
+    blue: '\u001b[34m',
+    magenta: '\u001b[35m',
+    cyan: '\u001b[36m',
+    white: '\u001b[37m',
+    brightBlack: '\u001b[30;1m',
+    brightRed: '\u001b[31;1m',
+    brightGreen: '\u001b[32;1m',
+    brightYellow: '\u001b[33;1m',
+    brightBlue: '\u001b[34;1m',
+    brightMagenta: '\u001b[35;1m',
+    brightCyan: '\u001b[36;1m',
+    brightWhite: '\u001b[37;1m',
+    reset: '\u001b[0m',
+  }
+  term.write(colorCodes[colorName])
+}
