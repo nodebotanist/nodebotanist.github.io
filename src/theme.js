3f87f5035fe0e63fa02775ad38ff92f853bc1c58
Kassandra Perch <the@nodebotani.st>
2018-12-13 11:16:28 -0600

    Colors in place, background/foreground set

---
+++ src/theme.js
@@ -0,0 +1,22 @@
+const colors = require('./colors')
+
+module.exports = {
+  blue: colors.blue,
+  brightBlue: colors.blue_highlight,
+  black: colors.black,
+  brightBlack: colors.black_dark,
+  cyan: colors.steel,
+  brightCyan: colors.steel_highlight,
+  green: colors.green,
+  brightGreen: colors.green_highlight,
+  magenta: colors.purple,
+  brightMagenta: colors.purple_highlight,
+  red: colors.red,
+  brightRed: colors.red_highlight,
+  white: colors.white,
+  brightWhite: colors.grey_light,
+  yellow: colors.yellow,
+  brightYellow: colors.yellow_highlight,
+  background: colors.black,
+  foreground: colors.white
+}
