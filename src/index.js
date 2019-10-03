c132372c4bb27f66872acc767fabce73b7b1e979
nodebotanist <the@nodebotani.st>
2019-10-03 07:01:48 -0500

    Fixed some stuff

--- src/index.js
+++ src/index.js
@@ -17,39 +17,39 @@ let terminalOptions = {
   theme,
   fontFamily: "Fira Mono",
   fontWeight: 400,
-  cols: 120
+  cols: 120,
+  textarea: '#input'
 }
 
-console.log(BrowserTerminal)
+
 
 let term = new Terminal(terminalOptions)
 let fitAddon = new fit()
-term.loadAddon(fitAddon)
 // term.loadAddon(WebfontLoader)
 
+
 let terminal = new BrowserTerminal(term)
-term.open(document.getElementById('#terminal'))
+
+terminal.open(document.getElementById('#terminal'))
+terminal.loadAddon(fitAddon)
 
 fitAddon.fit()
 
 // Intro time!
-term.write(`${terminal.format.newLine()}${terminal.format.text('cyan')}     *     ${terminal.format.newLine()}     |     ${terminal.format.newLine()}***********${terminal.format.newLine()}**       **${terminal.format.newLine()}**  ^ ^  **${terminal.format.newLine()}**   o   **${terminal.format.newLine()}**       **${terminal.format.newLine()}***********${terminal.format.newLine()}${terminal.format.reset('all')}Welcome to ${terminal.format.brightText('green')}https://nodebotani.st!${terminal.format.reset('all')}${terminal.format.newLine()}This is my personal portfolio site, as well as a terminal you can${terminal.format.newLine()}${terminal.format.text('magenta')}control some of my robotics projects${terminal.format.reset('all')} from!${terminal.format.newLine()}Use the ${terminal.format.brightText('magenta')}'help'${terminal.format.reset('all')} command to see all available commands ${terminal.format.newLine()} `)
+terminal.write(`${terminal.format.newLine()}${terminal.format.text('cyan')}     *     ${terminal.format.newLine()}     |     ${terminal.format.newLine()}***********${terminal.format.newLine()}**       **${terminal.format.newLine()}**  ^ ^  **${terminal.format.newLine()}**   o   **${terminal.format.newLine()}**       **${terminal.format.newLine()}***********${terminal.format.newLine()}${terminal.format.reset('all')}Welcome to 🤖  ${terminal.format.brightText('green')}https://nodebotani.st!${terminal.format.reset('all')}${terminal.format.newLine()}This is my personal portfolio site, as well as a terminal you can${terminal.format.newLine()}${terminal.format.text('magenta')}control some of my robotics projects${terminal.format.reset('all')} from!${terminal.format.newLine()}Use the ${terminal.format.brightText('magenta')}'help'${terminal.format.reset('all')} command to see all available commands ${terminal.format.newLine()} `)
 
-term.prompt()
+terminal.prompt()
+terminal.setup()
 // end Intro
 
-// term._core.register(term.addDisposableListener('paste', (data, ev) => {
-//   term.write(data)
-// }))
-
 terminal.dispatcher.on('help', (args) => {
-  terminal.terminal.write(helpCommand.run(args))
+  terminal.write(helpCommand.run(args))
 });
 
 terminal.dispatcher.on('whoarethey', (args) => {
-  terminal.terminal.write(whoAreTheyCommand.run())
+  terminal.write(whoAreTheyCommand.run())
 })
 
 terminal.dispatcher.on('githubActivity', async function() {
-  terminal.terminal.write(await githubActivityCommand.run())
+  terminal.write(await githubActivityCommand.run())
 })
