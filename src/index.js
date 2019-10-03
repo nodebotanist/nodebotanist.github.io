a66e5dbf0dd12f7bacd94f619af76193a29fd376
nodebotanist <the@nodebotani.st>
2019-10-03 06:20:06 -0500

    fixed fit addon

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Terminal = require('xterm').Terminal
-const fit = require('xterm/lib/addons/fit/fit')
+const fit = require('xterm-addon-fit').FitAddon
 const WebfontLoader = require('xterm-webfont')
 const BrowserTerminal = require('xterm-browser').Terminal
 
@@ -12,8 +12,6 @@ const githubActivityCommand = require('./commands/githubActivity')
 let currentLine = ''
 let currentLineLength = 0
 
-Terminal.applyAddon(fit)
-Terminal.applyAddon(WebfontLoader)
 
 let terminalOptions = {
   theme,
@@ -25,11 +23,14 @@ let terminalOptions = {
 console.log(BrowserTerminal)
 
 let term = new Terminal(terminalOptions)
-let terminal = new BrowserTerminal(term)
-term.loadWebfontAndOpen(document.getElementById('#terminal'))
-terminal.setup()
+let fitAddon = new fit()
+term.loadAddon(fitAddon)
+// term.loadAddon(WebfontLoader)
 
-// term.fit()
+let terminal = new BrowserTerminal(term)
+term.open(document.getElementById('#terminal'))
+
+fitAddon.fit()
 
 // Intro time!
 term.write(`${terminal.format.newLine()}${terminal.format.text('cyan')}     *     ${terminal.format.newLine()}     |     ${terminal.format.newLine()}***********${terminal.format.newLine()}**       **${terminal.format.newLine()}**  ^ ^  **${terminal.format.newLine()}**   o   **${terminal.format.newLine()}**       **${terminal.format.newLine()}***********${terminal.format.newLine()}${terminal.format.reset('all')}Welcome to ${terminal.format.brightText('green')}https://nodebotani.st!${terminal.format.reset('all')}${terminal.format.newLine()}This is my personal portfolio site, as well as a terminal you can${terminal.format.newLine()}${terminal.format.text('magenta')}control some of my robotics projects${terminal.format.reset('all')} from!${terminal.format.newLine()}Use the ${terminal.format.brightText('magenta')}'help'${terminal.format.reset('all')} command to see all available commands ${terminal.format.newLine()} `)
@@ -37,9 +38,9 @@ term.write(`${terminal.format.newLine()}${terminal.format.text('cyan')}     *   
 term.prompt()
 // end Intro
 
-term._core.register(term.addDisposableListener('paste', (data, ev) => {
-  term.write(data)
-}))
+// term._core.register(term.addDisposableListener('paste', (data, ev) => {
+//   term.write(data)
+// }))
 
 terminal.dispatcher.on('help', (args) => {
   terminal.terminal.write(helpCommand.run(args))
