67ab28e346cd44fcde7e4cb48d1cc82b39b275ae
nodebotanist <the@nodebotani.st>
2020-02-29 20:48:21 +0900

    Cleanup

--- src/commands/map.js
+++
@@ -1,9 +0,0 @@
-const helpCommand = require('./help')
-const whoAreTheyCommand = require('./whoarethey')
-const githubActivityCommand = require('./githubActivity')
-
-module.exports = {
-  help: helpCommand,
-  whoarethey: whoAreTheyCommand,
-  githubActivity: githubActivityCommand
-}
