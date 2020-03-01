041fba63a9f5e713137708183f3673c0a83ae2df
nodebotanist <the@nodebotani.st>
2020-03-01 16:18:09 +0900

    Header sorted, icon font

--- components/header.js
+++ components/header.js
@@ -4,6 +4,9 @@ export default function Header () {
   return (
     <Head>
       <link rel="stylesheet" href="/css/bootstrap.min.css" />
+      <link rel="stylesheet" href="/css/icofont.min.css" />
+      <title>Kassian Rosner Wren</title>
     </Head>
+
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,29 @@ export default function Index (){
   return (
     <>
       <Header></Header>
+      <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+        <a class="navbar-brand" href="/">Kassian Rosner Wren</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/resume">Resume</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/projects">Projects</a>
+            </li>
+          </ul>
+        </div>
+        <div class="navbar-nav">
+          <a class="icofont-twitter icofont-2x" target="_" href="https://twitter.com/nodebotanist" />
+          <a class="icofont-github icofont-2x" target="_" href="https://github.com/nodebotanist" />
+          <a class="icofont-linkedin icofont-2x" target="_" href="https://linkedin.com/in/nodebotanist" />
+        </div>
+      </nav>
       <div>
-        <p>Hello, Next.js!</p>
-        <Link href="profile">My profile</Link>
+
       </div>
     </>
   )
