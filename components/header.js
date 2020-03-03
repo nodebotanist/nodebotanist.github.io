76c189b2d96e342048a2b85acef53de4e7cd3406
nodebotanist <the@nodebotani.st>
2020-03-03 13:31:25 +0900

    Moving some pages around

--- components/header.js
+++ components/header.js
@@ -14,7 +14,7 @@ export default function Header () {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+              <a className="nav-link" href="#">Home</a>
             </li>
             <li className="nav-item">
               <a className="nav-link" href="/resume">Resume</a>
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,20 @@ export default function Index (){
   return (
     <>
       <Header></Header>
-      <div>
-
+      <div className="container main-body">
+        <div className="jumbotron">
+          <h1>Welcome!</h1>
+          <p>I'm Kassian Rosner Wren, formerly Kassandra Perch, and this is my portfolio site! This site was built with:</p>
+          <ul>
+            <li>Next.js</li>
+            <li>React</li>
+            <li>Bootstrap 4</li>
+            <li>Bootswatch Cyborg theme</li>
+            <li>GitHub Pages</li>
+          </ul>
+          <p>I have my projects listed on the projects page, and my resume on the resume page.</p>
+          <p>The status page has some widgets I've been working on.</p>
+        </div>
       </div>
     </>
   )
