dd42d8f1e6f2d5e79a59b161b8fc0003313f46aa
Kassian Wren <the@nodebotani.st>
2022-08-04 13:58:50 -0500

    Added first page and Gatsby scripts

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+const Page = ({serverData}) => { return <main><pre>{JSON.stringify(serverData, null, 2)}</pre><h1>Hello from the server!</h1></main> }
+export default Page
+
+export async function getServerData() {
+    // using fetch
+    let tweets = await(await fetch('https://paulieapi.gatsbyjs.io/api/get-latest-tweets', {
+        method: 'POST',
+        body: JSON.stringify({ id: 470012453 }),
+    })).json() 
+    return { props: {
+        tweets
+    }} 
+}
