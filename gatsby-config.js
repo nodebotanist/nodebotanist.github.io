692cc4cac7865fec083700e33067ed9e7955d97e
Kassian Wren <the@nodebotani.st>
2022-08-04 16:56:03 -0500

    Added kutty components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,3 @@
-/** @type {import('tailwindcss').Config} */
+
 module.exports = {
   }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,27 @@
 import React from "react"
-const Page = ({serverData}) => { return <main class="bg-slate-800"><pre>{JSON.stringify(serverData, null, 2)}</pre><h1>Hello from the server!</h1></main> }
+const Page = ({serverData}) => { 
+    return <main class="bg-slate-800">
+        <pre>{JSON.stringify(serverData, null, 2)}</pre>
+        <div class="grid grid-cols-1 gap-3 md:grid-cols-3">
+  <div class="card">
+    <div class="card-header">Normal Card</div>
+    <div class="card-body">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+    </div>
+    <div class="justify-end card-footer">
+      <a href="#" class="btn btn-link btn-sm">Cancel</a>
+      <a href="#" class="btn btn-light-primary btn-sm">Sure</a>
+    </div>
+  </div>
+  <a class="card" href="#">
+    <div class="card-header">Card as Link</div>
+    <div class="card-body">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+    </div>
+  </a>
+</div>
+        <h1>Hello from the server!</h1>
+    </main> }
 export default Page
 
 export async function getServerData() {
